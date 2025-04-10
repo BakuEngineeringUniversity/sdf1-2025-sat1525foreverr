f8f12601f20dd9291395883ecf28f24c8453eb05
sat1525foreverr <sat1525forever@gmail.com>
2025-04-10 13:38:32 +0400

    commit

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1000,
+    height: 1000,
+   
   })
 
   win.loadFile('index.html')
